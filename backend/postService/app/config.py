5346f19fc763950bdcb88ee5b8eeed46193ac902
Akshath Kamath <akshathkamathwork@gmail.com>
2025-09-23 00:58:06 -0400

    Added pymongo connection

---
+++ backend/postService/app/config.py
@@ -0,0 +1,31 @@
+# app/config.py
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+# Explicitly load .env file BEFORE creating Settings
+load_dotenv()
+
+class Settings(BaseSettings):
+    ## app
+    app_name: str = "Post Service"
+    debug: bool = True
+
+    ## redis
+    redis_url: str
+
+    ## mongo
+    mongo_url: str
+
+    ## jwt
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    jwt_access_token_expire_minutes: int = 30
+    jwt_refresh_token_expire_days: int = 7
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+
+# Create instance
+settings = Settings()
---
+++ backend/postService/app/db/mongo.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+from app.config import settings
+
+mongo_client: MongoClient = MongoClient(settings.mongo_url)
+
+def get_mongo() -> MongoClient:
+    return mongo_client
+
+get_mongo()
